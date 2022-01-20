14814167372b0fefb07c94e6cd2e77143b3d9a20
Abhishek Aman <69054530+selectivegravity@users.noreply.github.com>
2022-01-20 16:20:43 +0530

    Create Pairs of Songs With Total Durations Divisible by 60.cpp

---
+++ Array & Hashing/Medium/Pairs of Songs With Total Durations Divisible by 60.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60/
+
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int count=0;
+        vector <int> m(60,0);
+        for(int n:time){
+            n%=60;
+            if(n==0) count+=m[0]; 
+            else count+=m[60-n];
+            m[n]++;
+        }
+        return count;
+    }
+};
